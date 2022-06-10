e1731530059563176a9cbbbf01423f9e888a9a7b
Balaji Gopal <36466689+Balaji-Gangai@users.noreply.github.com>
2022-06-10 20:23:21 +0530

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,16 @@
+var mongoose  =  require('mongoose');  
+   
+var excelSchema = new mongoose.Schema({ 
+     
+    name:{  
+        type:String  
+    },  
+    email:{  
+        type:String  
+    },    
+    age:{  
+        type:Number  
+    }
+});  
+   
+module.exports = mongoose.model('userModel',excelSchema);  
